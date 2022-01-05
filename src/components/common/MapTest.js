deef0ec44f5827b0549c719c08ab85c5fab3687f
Duncan Browne <duncan.b.browne@gmail.com>
2022-01-05 14:40:39 +0000

    maps test data

--- src/components/common/MapTest.js
+++ src/components/common/MapTest.js
@@ -1,3 +1,13 @@
+
+const trip = {
+  memories: [
+    { location: { lat: -25.344, lng: 131.036 } },
+    { location: { lat: -26.344, lng: 130.036 } },
+    { location: { lat: -24.344, lng: 131.036 } },
+    { location: { lat: -25.344, lng: 132.036 } }
+  ],
+}
+
 function MapTest () {
   return (
     'Im a map'
