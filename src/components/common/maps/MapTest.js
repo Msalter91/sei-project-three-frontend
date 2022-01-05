e05ae7ac94c1c605a20d15aa6265631df8163805
Duncan Browne <duncan.b.browne@gmail.com>
2022-01-05 18:36:21 +0000

    center map on center of trip

--- src/components/common/maps/MapTest.js
+++ src/components/common/maps/MapTest.js
@@ -9,16 +9,27 @@ const trip = {
     { location: { lat: -25.344, lng: 132.036 } }
   ],
 }
+function findCenterOfLocationArray (array){
+  const locationSum = array.reduce((acc, cur)=>{
+    acc.lat += cur.location.lat
+    acc.lng += cur.location.lng
+    return acc
+  }, { lat: 0, lng: 0 })
+  const count = array.length
+  return { lat: locationSum.lat / count, lng: locationSum.lng / count }
+}
 
 function MapTest ({ tripData = trip, zoom = 4 , width = 500, height = 500 }) {
   const render = (status = Status) => {
     return <h1>{status}</h1>
   }
+
+  const tripCenter = findCenterOfLocationArray(tripData.memories)
   
   return (
     <div style={{ width: width, height: height }}>
       <Wrapper apiKey={process.env.REACT_APP_MAPS_API_KEY} render={render}>
-        <RenderMap center={tripData.memories[0].location} zoom={zoom} />
+        <RenderMap center={tripCenter} zoom={zoom} />
       </Wrapper>
     </div>
   )
