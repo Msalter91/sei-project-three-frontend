ec18ca595fb9fc527a1bd432ac8c5f78db6d6a2a
Duncan Browne <duncan.b.browne@gmail.com>
2022-01-06 16:35:55 +0000

    refactor marker statistics

--- src/components/common/maps/RenderMap.js
+++ src/components/common/maps/RenderMap.js
@@ -1,24 +1,42 @@
 import React, { useState } from 'react'
 import ReactMapGL, { Marker } from 'react-map-gl'
 
-function findCenterOfLocationArray (array){
-  const locationSum = array.reduce((acc, cur)=>{
+function getLocationArrayStats (array){
+  const locationAggregates = array.reduce((acc, cur)=>{
     acc.lat += cur.lat
     acc.lng += cur.lng
+    if (cur.lat > acc.latMax) acc.latMax = cur.lat
+    if (cur.lat < acc.latMin) acc.latMin = cur.lat
+    if (cur.lng > acc.lngMax) acc.lngMax = cur.lng
+    if (cur.lng < acc.lngMin) acc.lngMin = cur.lng
     return acc
-  }, { lat: 0, lng: 0 })
+  }, { lat: 0, lng: 0 , latMax: -180, latMin: 180, lngMax: -180, lngMin: 180 })
   const count = array.length
-  return { lat: locationSum.lat / count, lng: locationSum.lng / count }
+  return { 
+    lat: locationAggregates.lat / count, 
+    lng: locationAggregates.lng / count , 
+    latRng: locationAggregates.latMax - locationAggregates.latMin ,
+    lngRng: locationAggregates.lngMax - locationAggregates.lngMin ,
+  }
 }
 
 function RenderMap ({ 
   data, 
-  center = findCenterOfLocationArray(data.memories), 
+  center,
+  initZoom,
 }) {
+  const locationStats = getLocationArrayStats(data.memories)
+  if (!center){
+    center = { lat: locationStats.lat, lng: locationStats.lng }
+  }
+  if (!initZoom) {
+    // const latZoom = 2
+    initZoom = 3
+  }
   const [viewport, setViewport] = useState({
     latitude: center.lat,
     longitude: center.lng,
-    zoom: 3,
+    zoom: initZoom,
   })
 
   return (
