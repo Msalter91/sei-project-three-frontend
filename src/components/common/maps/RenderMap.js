757ecb8b02b8a2f0499d43dc8f5ac9a1a6e90d4c
Duncan Browne <duncan.b.browne@gmail.com>
2022-01-06 15:49:18 +0000

    remove mapbox package

--- src/components/common/maps/RenderMap.js
+++ src/components/common/maps/RenderMap.js
@@ -1,7 +1,4 @@
-import React, { useRef, useState, useEffect } from 'react'
-import mapbox from '!mapbox-gl' // eslint-disable-line
-
-mapbox.accessToken = process.env.REACT_APP_MAPS_API_KEY
+import React from 'react'
 
 function findCenterOfLocationArray (array){
   const locationSum = array.reduce((acc, cur)=>{
@@ -17,39 +14,11 @@ function RenderMap ({
   data, 
   center = findCenterOfLocationArray(data.memories), 
 }) {
-  const mapContainer = useRef(null)
-  const map = useRef(null)
-  const [lng, setLng] = useState(center.lng)
-  const [lat, setLat] = useState(center.lat)
-  const [zoom, setZoom] = useState(3)
-
-  useEffect(() => {
-    if (map.current) return // initialize map only once
-    map.current = new mapbox.Map({
-      container: mapContainer.current,
-      style: 'mapbox://styles/mapbox/streets-v11',
-      center: [lng, lat],
-      zoom: zoom,
-    })
-  })
-
-  useEffect(() => {
-    if (!map.current) return // wait for map to initialize
-    map.current.on('move', () => {
-      setLng(map.current.getCenter().lng.toFixed(4))
-      setLat(map.current.getCenter().lat.toFixed(4))
-      setZoom(map.current.getZoom().toFixed(2))
-    })
-  })
-
+  
   return (
     <div style={{ position: 'relative', height: '100%', width: '100%' }}>
-      <div className="sidebar">
-Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
-      </div>
-      <div ref={mapContainer} className='map-container'
-        style={{ position: 'absolute', left: 0, top: 0,height: '100%', width: '100%' }
-        }/>
+      {center.lat}
+      {center.lng}
     </div>
   )
 }
