1c38c7e99f2860006562a8a4b1cc4cf47da464cf
Duncan Browne <duncan.b.browne@gmail.com>
2022-01-06 14:46:25 +0000

    location array stats

--- src/components/common/maps/RenderMap.js
+++ src/components/common/maps/RenderMap.js
@@ -3,25 +3,44 @@ import mapbox from '!mapbox-gl' // eslint-disable-line
 
 mapbox.accessToken = process.env.REACT_APP_MAPS_API_KEY
 
-function findCenterOfLocationArray (array){
-  const locationSum = array.reduce((acc, cur)=>{
+function getLocationArrayStats (array){
+  const locationAggregates = array.reduce((acc, cur)=>{
     acc.lat += cur.lat
     acc.lng += cur.lng
+    if (cur.lat > acc.latMax) acc.latMax = cur.lat
+    if (cur.lat < acc.latMin) acc.latMin = cur.lat
+    if (cur.lng > acc.lngMax) acc.lngMax = cur.lng
+    if (cur.lng > acc.lngMax) acc.lngMax = cur.lng
     return acc
-  }, { lat: 0, lng: 0 })
+  }, { lat: 0, lng: 0 , latMax: -180, latMin: 180, lngMax: -180, lngMin: 180 })
   const count = array.length
-  return { lat: locationSum.lat / count, lng: locationSum.lng / count }
+  return { 
+    lat: locationAggregates.lat / count, 
+    lng: locationAggregates.lng / count , 
+    latRng: locationAggregates.latMax - locationAggregates.latMin ,
+    lngRng: locationAggregates.lngMax - locationAggregates.lngMin ,
+  }
 }
 
 function RenderMap ({ 
   data, 
-  center = findCenterOfLocationArray(data.memories), 
+  center, 
+  initZoom,
 }) {
+  const locationStats = getLocationArrayStats(data.memories)
+  if (!center){
+    center = { lat: locationStats.lat, lng: locationStats.lng }
+  }
+  if (!initZoom) {
+    const latZoom = 2
+    initZoom = 3
+  }
+
   const mapContainer = useRef(null)
   const map = useRef(null)
   const [lng, setLng] = useState(center.lng)
   const [lat, setLat] = useState(center.lat)
-  const [zoom, setZoom] = useState(3)
+  const [zoom, setZoom] = useState(initZoom)
 
   useEffect(() => {
     if (map.current) return // initialize map only once
