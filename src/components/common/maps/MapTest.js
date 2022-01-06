1e076e0e1a1f94ec62f8fb4840cfa3a91e513573
Duncan Browne <duncan.b.browne@gmail.com>
2022-01-06 13:39:43 +0000

    switch maps provider to mapbox

--- src/components/common/maps/MapTest.js
+++ src/components/common/maps/MapTest.js
@@ -1,4 +1,3 @@
-import { Wrapper, Status } from '@googlemaps/react-wrapper'
 import RenderMap from './RenderMap'
 
 const trip = {
@@ -9,28 +8,14 @@ const trip = {
     { lat: -28.344, lng: 138.036 }
   ],
 }
-function findCenterOfLocationArray (array){
-  const locationSum = array.reduce((acc, cur)=>{
-    acc.lat += cur.lat
-    acc.lng += cur.lng
-    return acc
-  }, { lat: 0, lng: 0 })
-  const count = array.length
-  return { lat: locationSum.lat / count, lng: locationSum.lng / count }
-}
 
-function MapTest ({ tripData = trip, zoom = 4 , width = 500, height = 500 }) {
-  const render = (status = Status) => {
-    return <h1>{status}</h1>
-  }
-
-  const tripCenter = findCenterOfLocationArray(tripData.memories)
+function MapTest ({ tripData = trip }){
   
   return (
-    <div style={{ width: width, height: height }}>
-      <Wrapper apiKey={process.env.REACT_APP_MAPS_API_KEY} render={render}>
-        <RenderMap center={tripCenter} zoom={zoom} />
-      </Wrapper>
+    <div style={{ height: 500, width: 500 }}>
+      <RenderMap 
+        data={tripData} 
+      />
     </div>
   )
 }
--- src/components/common/maps/RenderMap.js
+++ src/components/common/maps/RenderMap.js
@@ -1,23 +1,40 @@
-import React from 'react'
+import React, { useRef, useState, useEffect } from 'react'
+import mapbox from '!mapbox-gl' // eslint-disable-line
 
-function RenderMap ({ ...options }) {
-  const mapContainerRef = React.useRef(null)
-  const [map, setMap] = React.useState(null)
+mapbox.accessToken = process.env.REACT_APP_MAPS_API_KEY
 
-  React.useEffect(()=>{
-    if (mapContainerRef.current && !map) {
-      setMap(new window.google.maps.Map(mapContainerRef.current, {}))
-    }
-  },[mapContainerRef, map])
+function findCenterOfLocationArray (array){
+  const locationSum = array.reduce((acc, cur)=>{
+    acc.lat += cur.lat
+    acc.lng += cur.lng
+    return acc
+  }, { lat: 0, lng: 0 })
+  const count = array.length
+  return { lat: locationSum.lat / count, lng: locationSum.lng / count }
+}
 
-  if (map) {
-    map.setOptions(options)
-  }
+function RenderMap ({ 
+  data, 
+  center = findCenterOfLocationArray(data.memories), 
+}) {
+  const mapContainer = useRef(null)
+  const map = useRef(null)
+  const [lng, setLng] = useState(center.lng)
+  const [lat, setLat] = useState(center.lat)
+  const [zoom, setZoom] = useState(3)
+
+  useEffect(() => {
+    if (map.current) return // initialize map only once
+    map.current = new mapbox.Map({
+      container: mapContainer.current,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [lng, lat],
+      zoom: zoom,
+    })
+  })
 
   return (
-    <div ref={mapContainerRef} 
-      style= { { width: '100%', height: '100%' }}>
-    </div>
+    <div ref={mapContainer} className='map-container' style={{ height: '100%', width: '100%' }} />
   )
 }
 
