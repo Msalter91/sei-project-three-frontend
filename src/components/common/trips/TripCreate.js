261c16da8d829034922b8cdc7803ad555e89ed1e
Duncan Browne <duncan.b.browne@gmail.com>
2022-01-07 18:25:34 +0000

    control inputs with React

--- src/components/common/trips/TripCreate.js
+++ src/components/common/trips/TripCreate.js
@@ -1,3 +1,5 @@
+import React from 'react'
+
 import RenderMap from '../maps/RenderMap'
 
 const maxLengthTitle = 50
@@ -12,11 +14,25 @@ const initialState = {
 }
 
 function TripCreate () {
+  const [formData, setFormData] = React.useState(initialState)
+  const [formErrors, setFormErrors] = React.useState(initialState)
+  
+  const handleChange = e =>{
+    const value = e.target.value
+    setFormData({ ...formData, [e.target.name]: value })
+    console.log(formData)
+  }
+
+  const handleSubmit = async e =>{
+    e.preventDefault()
+    console.log('submitting:', formData)
+  }
+
   return (
     <section className="section">
       <form
         className="container-fluid row"
-        // onSubmit={handleSubmit}
+        onSubmit={handleSubmit}
       >
         <div className="col-7" 
           style={{ background: 'antiquewhite' }}>
@@ -26,7 +42,10 @@ function TripCreate () {
               type="text"
               name="title"
               id="title"
-              className="form-control"></input>
+              className="form-control"
+              value={formData.title}
+              onChange={handleChange}
+            />
           </div>
           <div className="form-group">
             <label htmlFor="countryVisited">Where did you start?</label>
@@ -34,14 +53,22 @@ function TripCreate () {
               type="text" 
               name="countryVisited"
               id="countryVisited"
-              className="form-control"></input>
+              className="form-control"
+              value={formData.countryVisited}
+              onChange={handleChange} 
+            />
           </div>
           <div className="form-group">
             <label htmlFor="notes">Tell the world about your trip!</label>
             <textarea 
               name="notes"
               id="notes"
-              className="form-control"></textarea>
+              className="form-control"
+              value={formData.notes}
+              onChange={handleChange} />
+            <div className='row'>
+              <small className="form-text text-muted ml-auto text-end">{maxLengthNotes - formData.notes.length} characters remaining</small>
+            </div>
           </div>
           <div>
             <RenderMap />
