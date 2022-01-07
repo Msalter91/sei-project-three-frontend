bc821732e3a67f65eb3ecaa3d086a4e79d1bf681
achan81 <achan81@icloud.com>
2022-01-07 15:10:46 +0000

    registration page now working ok

--- src/components/common/Profile.js
+++ src/components/common/Profile.js
@@ -1,6 +1,29 @@
 function Profile() {
   return (
-    <p>profile</p>
+
+    <><div className="profile-page">
+
+      {/* <img src="https://res.cloudinary.com/team-mad/image/upload/v1641486472/placebook-country-images/ATA_tta90o.jpg"
+        className="banner" alt="Antarctica-img" /> */}
+
+      <img src="https://imgur.com/PBLhrzp.jpg" className="img-fluid banner" alt="Antarctica-img" />
+      <h5 className="home-subtitle">travel, capture, share</h5>
+
+
+
+
+
+
+    </div><p>profile</p></>
+
+
+
+
+
+
+
+
+
   )
 }
 
