5cb7a4cdb8029a1639ab8f428a3952937145b673
Duncan Browne <duncan.b.browne@gmail.com>
2022-01-07 16:18:43 +0000

    new trip page layout

--- src/components/common/trips/TripCreate.js
+++ src/components/common/trips/TripCreate.js
@@ -1,3 +1,4 @@
+import RenderMap from '../maps/RenderMap'
 
 const maxLengthTitle = 50
 const maxLengthNotes = 300
@@ -19,11 +20,35 @@ function TripCreate () {
       >
         <div className="col-7" 
           style={{ background: 'antiquewhite' }}>
-            
+          <div className="form-group">
+            <label htmlFor="tripTitle"></label>
+            <input 
+              type="text" 
+              id="tripTitle"
+              className="form-control"></input>
+          </div>
+          <div className="form-group">
+            <label htmlFor="tripCountry">Where did you start?</label>
+            <input 
+              type="text" 
+              id="tripCountry"
+              className="form-control"></input>
+          </div>
+          <div className="form-group">
+            <label htmlFor="tripNotes">Tell the world about your trip!</label>
+            <textarea 
+              id="tripNotes"
+              className="form-control"></textarea>
+          </div>
+          <div>
+            <RenderMap />
+          </div>
         </div>
         <div className="col"
-          style={{ background: 'khaki' }}
-        >right</div>
+          style={{ background: 'khaki' }}>
+          {/* TODO: create memory component */}
+          {/* TODO: add new create memory component */}
+        </div>
       </form>
     </section>
   )
