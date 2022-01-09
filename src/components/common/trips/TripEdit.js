8af1831eaa659882aa8120eae65a0f9770a8ec83
Duncan Browne <duncan.b.browne@gmail.com>
2022-01-09 10:42:29 +0000

    Create Trip page, see branch 'trip-create-page'

--- src/components/common/trips/TripEdit.js
+++ src/components/common/trips/TripEdit.js
@@ -1,8 +1,11 @@
-import React from 'react'
-import { tripEdit } from '../../../lib/api'
+import { useState, useEffect } from 'react'
+import { tripGetById, tripEdit } from '../../../lib/api'
 import { useParams } from 'react-router-dom'
 
 import RenderMap from '../maps/RenderMap'
+import Error from '../Error'
+import MemoryEdit from '../memories/MemoryEdit'
+import MemoryCreate from '../memories/MemoryCreate'
 
 const maxLengthTitle = 50
 const maxLengthNotes = 300
@@ -16,18 +19,37 @@ const initialState = {
 }
 
 function TripEdit () {
-  const [formData, setFormData] = React.useState(initialState)
+  const [formData, setFormData] = useState(initialState)
   const notesRemainingChars = maxLengthNotes - formData.notes.length
-  const [formErrors, setFormErrors] = React.useState(initialState)
+  const [formErrors, setFormErrors] = useState(initialState)
   const { tripId } = useParams()
+  const [isError, setIsError] = useState(false)
+  // populate initial data
+
+  useEffect(()=>{
+    (async ()=>{
+      try {
+        const getTripRes = await tripGetById(tripId)
+        const newFormData = { ...formData,...getTripRes.data }
+        setFormData(newFormData)
+      } catch (err){
+        setIsError(true)
+      }
+    })()
+    // Do NOT want this hook to re-render on formData change, therefore accept non-exhaustive dependency
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [tripId])
   
   const handleChange = e =>{
     const value = e.target.value
-    // if user is editing title field and exceeds length, do not accept new characters.
-    // allow 
+    // if user is editing limited field and exceeds length, do not accept new characters.
+    // improves UX and ensures form can always be submitted so that updating memories does not get blocked by over-length text fields
+    // TODO: release lock on notes length to make writing easier, but cut off over-length writing on submit?
     if (
-      e.target.name === 'title' &&
-      e.target.value.length > maxLengthTitle
+      (e.target.name === 'title' &&
+      e.target.value.length > maxLengthTitle) ||
+      (e.target.name === 'notes' &&
+      e.target.value.length > maxLengthNotes)
     ) {
       return
     }
@@ -44,77 +66,99 @@ function TripEdit () {
       setFormErrors(err.response.data.errors)
     }
   }
+  // const newMemoryEditor = async ()=>{
+  //   console.log('new mem button')
+  // }
   return (
     <section className="section">
-      <form
-        className="container-fluid row"
-        onSubmit={handleSubmit}
-      >
-        <div className="col-7" 
-          style={{ background: 'antiquewhite' }}>
-          <div className="form-group">
-            <label htmlFor="title"></label>
-            <input 
-              type="text"
-              name="title"
-              id="title"
-              className={
-                `form-control ${formErrors.countryVisited ? 'border-danger' : ''}`}
-              value={formData.title}
-              onChange={handleChange}
-            />
-            {formErrors.title && <p className="text-danger">{formErrors.title}</p>  }
-          </div>
-          <div className="form-group">
-            <label htmlFor="countryVisited">Where did you start?</label>
-            <input 
-              type="text" 
-              name="countryVisited"
-              id="countryVisited"
-              className={
-                `form-control ${formErrors.countryVisited ? 'border-danger' : ''}`}
-              value={formData.countryVisited}
-              onChange={handleChange} 
-            />
-            {formErrors.countryVisited && <p className="text-danger">{formErrors.countryVisited}</p>  }
-          </div>
-          <div className="form-group">
-            <label htmlFor="notes">Tell the world about your trip!</label>
-            <textarea 
-              name="notes"
-              id="notes"
-              className={
-                `form-control 
+      {isError ? (
+        <Error />
+      ) : (
+        <div className='container-fluid row'>
+          <form
+            onSubmit={handleSubmit}
+            className="col-7" 
+            style={{ background: 'antiquewhite' }}
+          >
+            <div className="form-group">
+              <label htmlFor="title"></label>
+              <input 
+                type="text"
+                name="title"
+                id="title"
+                className={
+                  `form-control ${formErrors.countryVisited ? 'border-danger' : ''}`}
+                value={formData.title}
+                onChange={handleChange}
+              />
+              {formErrors.title && <p className="text-danger">{formErrors.title}</p>  }
+            </div>
+            <div className="form-group">
+              <label htmlFor="countryVisited">Where did you start?</label>
+              <input 
+                type="text" 
+                name="countryVisited"
+                id="countryVisited"
+                className={
+                  `form-control ${formErrors.countryVisited ? 'border-danger' : ''}`}
+                value={formData.countryVisited}
+                onChange={handleChange} 
+              />
+              {formErrors.countryVisited && <p className="text-danger">{formErrors.countryVisited}</p>  }
+            </div>
+            <div className="form-group">
+              <label htmlFor="notes">Tell the world about your trip!</label>
+              <textarea 
+                name="notes"
+                id="notes"
+                className={
+                  `form-control 
                 ${(notesRemainingChars < 0 ||
                   formErrors.notes ) ? 'border-danger' : ''}`}
-              value={formData.notes}
-              onChange={handleChange} />
-            <div className='row'>
-              <small 
-                className={`form-text ml-auto text-end ${
-                  (notesRemainingChars < 0) ? 'text-danger' : 'text-muted'
-                }`}
-              >{notesRemainingChars} characters remaining</small>
+                value={formData.notes}
+                onChange={handleChange} />
+              <div className='row'>
+                <small 
+                  className={`form-text ml-auto text-end ${
+                    (notesRemainingChars < 0) ? 'text-danger' : 'text-muted'
+                  }`}
+                >{notesRemainingChars} characters remaining</small>
+              </div>
+              {formErrors.notes && <p className="text-danger">{formErrors.notes}</p>  }
             </div>
-            {formErrors.notes && <p className="text-danger">{formErrors.notes}</p>  }
+            <div>
+              <RenderMap />
+            </div>
+            <div className='row'>
+              <button 
+                type="submit"
+                className="btn btn-success ml-auto"
+              >Save your trip</button>
+            </div>
+          </form>
+          <div 
+            className="col"
+            style={{ background: 'khaki' }}>
+            <div 
+              className='d-flex flex-column'>
+            </div>
+            <div className='custom-memories-container row'>
+              {Boolean(formData.memories.length) && formData.memories.map(
+                memory => <MemoryEdit key={memory._id} memory={memory}/>
+              )}
+            </div>
+            <div className='create-memory-container row' >
+              <MemoryCreate tripId={tripId}/>
+            </div>
+            {/* <div className='row'>
+              <button type="button" 
+                className="btn btn-primary"
+                onClick={newMemoryEditor}
+              >Add a Memory</button>
+            </div> */}
           </div>
-          <div>
-            <RenderMap />
-          </div>
         </div>
-        <div 
-          className="col d-flex flex-column"
-          style={{ background: 'khaki' }}>
-          {/* TODO: create memory component */}
-          {/* TODO: add new create memory component */}
-        </div>
-        <div className='row'>
-          <button 
-            type="submit"
-            className="btn btn-success ml-auto"
-          >Save your trip</button>
-        </div>
-      </form>
+      )}
     </section>
   )
 }
--- src/lib/api.js
+++ src/lib/api.js
@@ -32,7 +32,23 @@ export function tripCreate(formData){
   return axios.post(`${baseUrl}/trips/`, formData, authHeader())
 }
 
-
 export function tripEdit(tripId, formData){
   return axios.put(`${baseUrl}/trips/${tripId}`, formData, authHeader())
+}
+
+export function tripGetById(tripId){
+  return axios.get(`${baseUrl}/trips/${tripId}`)
+}
+
+// Memories
+export function memoryGetById(memoryId){
+  return axios.get(`${baseUrl}/memories/${memoryId}`)
+}
+
+export function memoryEdit(memoryId, formData){
+  return axios.put(`${baseUrl}/memories/${memoryId}`, formData, authHeader())
+}
+
+export function memoryCreate(formData){
+  return axios.post(`${baseUrl}/memories/`, formData, authHeader())
 }
