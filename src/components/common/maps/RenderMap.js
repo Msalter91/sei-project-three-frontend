8c4d681870ac30f927d611f29ec304324eb2dc3c
Duncan Browne <duncan.b.browne@gmail.com>
2022-01-09 15:28:35 +0000

    zoom map when memories change

--- src/components/common/maps/RenderMap.js
+++ src/components/common/maps/RenderMap.js
@@ -68,10 +68,11 @@ function RenderMap ({
       zoom: fittedZoom })
   }
   useEffect(()=>{
+    console.log('zooming')
     if (hasMemories){
       fitViewPort()
     }
-  }, [])
+  }, [data.memories])
 
   return (
     <div className="map-container" style={{ height: '100%', width: '100%' }}>
