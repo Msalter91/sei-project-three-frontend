0dc1936d0a23160a1e242f94a610168f7f74b3a3
Duncan Browne <duncan.b.browne@gmail.com>
2022-01-10 23:09:13 +0000

    location picker in memoryEdit

--- src/components/common/memories/memorySmallChildren/MemoryEdit.js
+++ src/components/common/memories/memorySmallChildren/MemoryEdit.js
@@ -3,6 +3,7 @@ import { useState } from 'react'
 import { memoryEdit } from '../../../../lib/api.js'
 import { logoImageLink } from '../../../../lib/config.js'
 import { uploadImageMemory } from '../../../../lib/imageHosting.js'
+import LocationPicker from '../../maps/LocationPicker.js'
 
 
 const initialState = {
@@ -50,17 +51,20 @@ function MemoryEdit ({ memory, handleSwitchToShow, updateClientsideMemory }) {
     }
   }
 
+  const captureLocation = (location) => {
+    setFormData({ ...formData, long: location[0], lat: location[1] })
+  }
+
   return (
     <form 
       className="container-fluid row"
-      onSubmit={handleSubmit}
-    >
-      <div className='col'>
-        <div className="form-group">
-          <label className="label" htmlFor="image" role='button'>
+      onSubmit={handleSubmit}>
+      <div className='row'>
+        <div className="form-group col">
+          <label className="label" htmlFor="image" role="button">
             <p>Share a photo?</p>
             <img 
-              src={formData.image ? formData.image : logoImageLink } 
+              src={formData.image ? formData.image : logoImageLink} 
               alt={formData.name} 
               className='memory-edit-image' 
             />
@@ -72,100 +76,61 @@ function MemoryEdit ({ memory, handleSwitchToShow, updateClientsideMemory }) {
             accept="image/png, image/jpeg"
             onChange={handleImageUpload} 
           />
-          {isUploadingImage && <p>Image uploading...</p>}
+          {isUploadingImage && <p>Image uploading</p>}
+        </div>
+        <div className="form-group col">
+          <label htmlFor="location">Where were you?</label>
+          <LocationPicker getLocationFromMap={captureLocation} />
         </div>
       </div>
-      <div className='col'>
-        <div className="form-group">
-          <label htmlFor="name">What happened?</label>
-          <input
-            type='text' 
-            name="name"
-            id="name"
-            className={
-              `form-control 
+
+      <div className="form-group">
+        <label htmlFor="name">What happened?</label>
+        <input
+          type='text' 
+          name="name"
+          id="name"
+          className={
+            `form-control 
                 ${(formErrors.name ) ? 'border-danger' : ''}
                 `}
-            value={formData.name}
-            onChange={handleChange} />
-          {formErrors.name && <p className="text-danger">{formErrors.name}</p>  }
-        </div>
-        <div className="form-group">
-          <label htmlFor="location">Where were you?</label>
-          <input
-            type='text' 
-            name="location"
-            id="location"
-            className={
-              `form-control 
-                ${(formErrors.name ) ? 'border-danger' : ''}
-                `}
-            value={formData.location}
-            onChange={handleChange} />
-          {formErrors.location && <p className="text-danger">{formErrors.location}</p>  }
-        </div>
-        <div className="form-group">
-          <label htmlFor="lat">LATITUDE</label>
-          <input
-            type='number' 
-            name="lat"
-            id="lat"
-            className={
-              `form-control 
-                ${(formErrors.lat ) ? 'border-danger' : ''}
-                `}
-            value={formData.lat}
-            onChange={handleChange} />
-          {formErrors.lat && <p className="text-danger">{formErrors.lat}</p>  }
-        </div>
-        <div className="form-group">
-          <label htmlFor="long">LONGITUDE</label>
-          <input
-            type='number' 
-            name="long"
-            id="long"
-            className={
-              `form-control 
-                ${(formErrors.long ) ? 'border-danger' : ''}
-                `}
-            value={formData.long}
-            onChange={handleChange} />
-          {formErrors.long && <p className="text-danger">{formErrors.long}</p>  }
-        </div>
-        <div className="form-group">
-          <label htmlFor="visitDate">When were you there?</label>
-          <input
-            type='date' 
-            name="visitDate"
-            id="visitDate"
-            className={
-              `form-control 
+          value={formData.name}
+          onChange={handleChange} />
+        {formErrors.name && <p className="text-danger">{formErrors.name}</p>  }
+      </div>
+      <div className="form-group">
+        <label htmlFor="visitDate">When were you there?</label>
+        <input
+          type='date' 
+          name="visitDate"
+          id="visitDate"
+          className={
+            `form-control 
                 ${(formErrors.visitDate ) ? 'border-danger' : ''}
                 `}
-            value={formData.visitDate}
-            onChange={handleChange} />
-          {Boolean(formErrors.visitDate) && <p className="text-danger">{formErrors.visitDate}</p>  }
-        </div>
-        <div className="form-group">
-          <label htmlFor="notes">What did you love?</label>
-          <textarea 
-            name="notes"
-            id="notes"
-            className={
-              `form-control 
+          value={formData.visitDate}
+          onChange={handleChange} />
+        {Boolean(formErrors.visitDate) && <p className="text-danger">{formErrors.visitDate}</p>  }
+      </div>
+      <div className="form-group">
+        <label htmlFor="notes">What did you love?</label>
+        <textarea 
+          name="notes"
+          id="notes"
+          className={
+            `form-control 
                 ${(notesRemainingChars < 0 ||
                   formErrors.notes ) ? 'border-danger' : ''}`}
-            value={formData.notes}
-            onChange={handleChange} />
-          <div className='row'>
-            <small 
-              className={`form-text ml-auto text-end ${
-                (notesRemainingChars < 0) ? 'text-danger' : 'text-muted'
-              }`}
-            >{notesRemainingChars} characters remaining</small>
-          </div>
-          {formErrors.notes && <p className="text-danger">{formErrors.notes}</p>  }
+          value={formData.notes}
+          onChange={handleChange} />
+        <div className='row'>
+          <small 
+            className={`form-text ml-auto text-end ${
+              (notesRemainingChars < 0) ? 'text-danger' : 'text-muted'
+            }`}
+          >{notesRemainingChars} characters remaining</small>
         </div>
+        {formErrors.notes && <p className="text-danger">{formErrors.notes}</p>  }
       </div>
       <div className='row'>
         {isUploadingImage && <p>Image uploading...</p>}
