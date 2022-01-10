1821ae67884a172fa8d2bb36137544b97a51b57c
achan81 <achan81@icloud.com>
2022-01-10 21:16:45 +0000

    fixed merge issue

--- src/components/Countries/Countries.js
+++ src/components/Countries/Countries.js
@@ -34,7 +34,6 @@ function Countries() {
   }
 
   return (
-<<<<<<< HEAD
 
     <div className="container">
       <div className="col-md-10 mx-auto"
@@ -85,38 +84,6 @@ function Countries() {
                       {/* <div className="card-body"> */}
                       {/* </div> */}
                       {/* </div> */}
-=======
-    <div className='container-fluid h-100'>
-      <div className='row header-wrapper'>
-        <div className='col'>
-          <div className='header-splash'>
-            <h2>Inspiration for your next memory</h2>
-          </div>
-        </div>
-      </div>
-      <div className='row'>
-        <div className='col'>
-          <div className='country-search'>
-            <input
-              placeholder='Search by country'
-              onChange={handleSearch}
-            >
-            </input>
-          </div>
-        </div>
-      </div>
-      <div className=' row card-container'>
-        {
-          countries && (
-            filteredCountries(countries).map(country=> {
-              return <div className='col-3' key={country.name}>
-                <Link to={`/countries/${country._id}`}>
-                  <div className="card">
-                    <img className='' src={country.image} alt="Card image cap"></ img>
-                    <div className="card-body">
-                      <h6 className="card-title">{country.name}</h6>
-                      <a href="#" className="btn btn-primary">Trips in {country.name}</a>
->>>>>>> development
                     </div>
                   </Link>
                 </div>
