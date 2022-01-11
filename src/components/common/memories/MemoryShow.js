5156814eb7ea51cf7bad5286b349adc8c4a6943f
Mike Salter <mikey.l.salter@gmail.com>
2022-01-11 13:32:29 +0000

    memory-show-functionality-working

--- src/components/common/memories/MemoryShow.js
+++ src/components/common/memories/MemoryShow.js
@@ -6,19 +6,26 @@ function MemoryShow () {
   const { memoryId } = useParams()
 
   const [memory, setMemory] = React.useState(null)
+  const [user, setUser] = React.useState(null)
+  const [trip, setTrip] = React.useState(null)
 
   React.useEffect(() => {
     const getMemory = async () => {
       try {
         const memoryData = await axios.get(`/api/memories/${memoryId}`)
         setMemory(memoryData.data)
+        setUser(memoryData.data.addedBy)
+        setTrip(memoryData.data.pairedTrip)
       } catch (err) {
         console.log(err)
       }
     }
     getMemory()
   }, [memoryId] )
-  console.log(memory)
+
+  const readableTime = new Date(Date.parse(memory ? memory.created_at : 1)).toLocaleString()
+
+  console.log(trip)
 
   return (
 
@@ -30,13 +37,13 @@ function MemoryShow () {
         <div className="bg-white shadow rounded overflow-hidden">
           <div className="px-4 pt-0 pb-4 cover">
             <div className="d-flex align-items-end profile-head">
-              <div className="profile mr-3"><img src="https://i.imgur.com/bEYsXdQ.jpg?1" alt="..." width="150" className="rounded mb-2 img-thumbnail"/>              
+              <div className="profile mr-3"><img src={user && user.image} alt="..." width="150" className="rounded mb-2 img-thumbnail"/>              
               </div>
 
               <div className="d-flex-body mb-5 text-white">
-                <h4 className="memsname text-uppercase mt-0 mb-0 ms-3">name</h4>
+                <h4 className="memsname text-uppercase mt-0 mb-0 ms-3">{user && user.displayName}</h4>
                 <p className="memslocation text-capitalize small mb-4">
-                  <i className="fas fa-map-marker-alt mr-2 ms-3"></i>location</p>
+                  <i className="fas fa-map-marker-alt mr-2 ms-3"></i>{user && user.location}</p>
               </div>
             </div>
           </div>
@@ -54,19 +61,25 @@ function MemoryShow () {
           <div className="fluid">
             {/* <div className="row"> */}
             <div className="memstitle gy-1 row row-col text-wrap text-center text-capitalize">
-              <h2>{memory.name}</h2>
-              <img src={memory.image}/>
+              <h2>{memory && memory.name}</h2>
+              <img src={memory && memory.image}/>
             </div>
 
             {/* </div> */}
           
             <div className="p-3 rounded shadow-sm bg-light">
               <div className="row memory-info-holder">
-                <div className="col-sm-4">
-                  <p>part of Trip</p>
-                </div>
+                
+                {trip && (
+                  <div className="col-sm-4">
+                    <p>Part of <Link to={`/trips/${trip._id}`}>
+                      {trip.title}
+                    </Link></p>
+                  </div>
+                )}
+                
                 <div className="col-sm-8">
-                  <p>Added on {memory.created_at}</p>
+                  <p>Added on {memory && readableTime} </p>
                 </div>
               </div>
             </div>
@@ -74,8 +87,8 @@ function MemoryShow () {
             <div className="p-3 rounded shadow-sm bg-light">
               <div className="row text-wrap text-center">
                 <div className="col-sm-12">
-                  <p>{memory.location}</p>
-                  <p>{memory.notes}</p>
+                  <p>{memory && memory.location}</p>
+                  <p>{memory && memory.notes}</p>
                 </div>
               </div>
 
@@ -84,7 +97,7 @@ function MemoryShow () {
               </div>
             </div>
             <button>
-              <Link to="/trips/edit"
+              <Link to={trip ? `/trips/${trip._id}/edit` : '/'}
                 className="btn btn-outline-info btn-sm d-flex justify-content-center">
                   Edit / Delete Memory</Link>
             </button>
