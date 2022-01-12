ea9578db618a04bd7dda6c0817412356b7b24867
Mike Salter <mikey.l.salter@gmail.com>
2022-01-12 16:17:59 +0000

    profile-shows-preview

--- src/lib/config.js
+++ src/lib/config.js
@@ -1,3 +1,5 @@
 export const logoImageLink = 'https://i.imgur.com/ElzRumx.png'
 
+export const profileImageLink = 'https://i.imgur.com/vX6eUuR.png'
+
 export const mapboxApiAccessToken = process.env.REACT_APP_MAPS_API_KEY
